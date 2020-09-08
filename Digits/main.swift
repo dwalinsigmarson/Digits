7689c82764333425f1673b154c464ae21a89fdf0
dwalinsigmarson <dwalinsigmarson@gmail.com>
2020-09-08 23:58:56 +0300

    Initial Commit

---
+++ Digits/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Digits
+//
+//  Created by Dmytro Davydenko on 08.09.2020.
+//  Copyright © 2020 Dmytro Davydenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
